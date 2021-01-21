eef289181649718ee619646936c234c01cf453a8
Jizga <jizga93@gmail.com>
2021-01-21 13:59:34 +0100

    Modal Form Añadir

--- js/WCEmpleados.js
+++ js/WCEmpleados.js
@@ -1,163 +1,229 @@
-class WCEmpleados extends HTMLElement {
-    constructor() {
-        super()
+//Script para los empleados
+
+
+class WCEmpleados extends HTMLElement{
+    constructor(){
+        super();
     }
 
-    connectedCallback() {
-        let shadowRoot = this.attachShadow({
-            mode: "open"
-        });
+    
+
+    connectedCallback(){
+    
+    
+
+        let shadowRoot = this.attachShadow({mode: "open"});
+
+        
+
         shadowRoot.innerHTML = `
+        <style>
+        .body {
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+            font-size: 1rem;
+            font-weight: 400;
+            line-height: 1.5;
+            color: #212529;
+            text-align: left;
+            background-color: #fff;
+          }
+        </style>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
         crossorigin="anonymous"></script>
-       
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
         crossorigin="anonymous"></script>
 
-        <div class="container-fluid">
+        <div class="container-fluid body">
             <div class="d-flex justify-content-between">
                 <h1 class="text text-dark mt-5 mb-5">EMPLEADOS</h1>
                 <div class="mt-5">
-                   
+                <div class="container">
+
+  <!-- Button to Open the Modal -->
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal" id="botonAdd">
+    Añadir Empleado
+  </button>
+
+  <!-- The Modal -->
+  <div class="modal" id="myModal">
+    <div class="modal-dialog">
+      <div class="modal-content">
+      
+        <!-- Modal Header -->
+        <div class="modal-header">
+          <h4 class="text-center tx">NUEVO EMPLEADO</h4>
+          <button type="button"  id ="cerrarModal" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        
+        <!-- Modal body -->
+        <div class="modal-body">
+        <div class="form-group">
+        <form action="">
+            <label>Nombre:</label> <input type="text" class="form-control" placeholder="Nombre" required id="nuevoNombre"><br>
+            <label>Apellidos:</label> <input type="text" class="form-control" placeholder="Apellidos" required id="nuevoApellido"><br>
+            <label>DNI</label><input type="text" class="form-control" placeholder="DNI" required id="nuevoDNI"><br>
+            <label>Fecha de alta</label><input type="date"  class="form-control" required id="nuevoFecha"><br>
+        </form>
+    </div>
+        </div>
+        
+        <!-- Modal footer -->
+        <div class="modal-footer">
+            <button type="button" id="cerrarModal1" class="btn btn-dark" data-dismiss="modal">Cancelar</button>
+            <button type="button" id="guardarEmpleado" class="btn btn-warning" data-dismiss="modal">Guardar</button>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+  
+</div>
+                        
+                
                 </div>
             </div>
             
            
             <table id="tablaDatos" class="table"></table>
+            
+        </div> 
+        </div> 
+        `
+        shadowRoot.querySelector("#botonAdd").addEventListener("click",()=>this.verModal())
+        shadowRoot.querySelector("#cerrarModal").addEventListener("click",()=>this.cerrarModal())
+        shadowRoot.querySelector("#cerrarModal1").addEventListener("click",()=>this.cerrarModal())
 
-<!--ÁREA DEL MODAL-->
-          
-    <!-- Button to Open the Modal -->
-    <button type="button" class="btn btn-primary" data-toggle="modal"  data-target="#myModal" id="bmodal">
-        Open modal
-    </button>
-
-    <!-- The Modal -->
-    <div class="modal" id="myModal">
-        <div class="modal-dialog">
-            <div class="modal-content">
-
-               
-
-                <!-- Modal body -->
-                <div class="modal-body">
-                    Modal body..
-                </div>
-
-               
-
-            </div>
-        </div>
-    </div>
-        </div> `
+        
+        //Obtener datos JSON
+        var datosJSON = getDatos("datos/empleados.json");
 
 
-        shadowRoot.querySelector("#bmodal").addEventListener("click",this.verModal())
-        //Datos del JSON, con la promesa
-        let datosJSON = getDatos("./datos/empleados.json")
+        datosJSON.then((dato) =>{
 
-        //Manejo de los datos del JSON 
-        datosJSON
-            .then((dato) => {
+            let arrayDatosJSON = dato;
 
-                let arrayDatosJSON = dato //(DATOS SIN PROMESA) ***********************************
+            let propiedadesTablaEmpleados = Object.keys(dato[0]);
 
-                let propiedadesTablaEmpleados = Object.keys(dato[0])
-                let tablaDatos = shadowRoot.querySelector("#tablaDatos")
+            let tablaDatos = shadowRoot.querySelector("#tablaDatos");
 
-                //CREAR EL "thead" DE LA TABLA DE FORMA DINÁMICA: ********
-
-                let thead = document.createElement("thead")
-                thead.classList.add("btn-dark")
-                let thIcono = document.createElement("th")
-                thIcono.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+            let thead = document.createElement("thead");
+            thead.classList.add("bg-dark")
+            thead.classList.add("text-light")
+            
+            let thIcono = document.createElement("th")
+            thIcono.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                 </svg>`
-                thead.appendChild(thIcono)
+            thead.appendChild(thIcono)
+            
+            propiedadesTablaEmpleados.forEach(propiedad =>{
+                if(propiedad !== "jornada"){
+                    //console.log(propiedad)
+                    let th = document.createElement("th");
+                    propiedad = propiedad.replace("_", " ");
+                    th.innerHTML = propiedad.toUpperCase();
+                    thead.appendChild(th);
+                    //console.log(propiedad)
+                }
+            })
+            tablaDatos.appendChild(thead);
+            let thEditar = document.createElement("th");
+            thEditar.innerHTML = "";
+            thead.appendChild(thEditar);
+            let tbody = document.createElement("tbody");
+            tablaDatos.appendChild(tbody);
 
-                propiedadesTablaEmpleados.forEach(propiedad => {
-                    if (propiedad !== "jornada") {
+            console.log(arrayDatosJSON)
 
-                        propiedad = propiedad.replace("_", " ")
+            arrayDatosJSON.forEach(empleado =>{
+                let tr = document.createElement("tr");
+                tbody.appendChild(tr);
 
-                     console.log(propiedad)
+                let datoIcono = document.createElement("td");
+                tr.appendChild(datoIcono);
+                datoIcono.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                </svg>`;
 
-                        let th = document.createElement("th")
-                        th.innerHTML = propiedad.toUpperCase()
-                        thead.appendChild(th)
-                    }
-                })
-
-                let thEditar = document.createElement("th")
-                thEditar.innerHTML = ""
-                thead.appendChild(thEditar)
-
-                tablaDatos.appendChild(thead)
-
-
-                //CREAR EL "tbody" DE LA TABLA DINÁMICAMENTE: *******
-
-                arrayDatosJSON.forEach(dato => {
-
-                    let tr = document.createElement("tr")
-                    let tdIcono = document.createElement("th")
-                    tdIcono.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                    </svg>`
-                    tr.appendChild(tdIcono)
-
-                    for (const property in dato) {
-
-                        if (dato.fecha_baja === null) {
-                            dato.fecha_baja = "-"
-                        }
-
-                        if (property !== null && property !== "jornada") {
-                            //console.log(property)
-                            let td = document.createElement("td")
-                            td.innerHTML = dato[property]
-
-                            tr.appendChild(td)
-                        }
+                for(let propiedad in empleado){
+                    if (empleado.fecha_baja == null){
+                        empleado.fecha_baja = "-"
                     }
 
-                    let tdEditar = document.createElement("td");
-                    tdEditar.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-                    </svg>`
-                    tdEditar.addEventListener("click", () => alert("MODIFICAR EMPLEADO"))
-                    tr.appendChild(tdEditar)
+                    if( propiedad !== null && propiedad !== "jornada"){
+                        let td = document.createElement("td");
+                        td.innerHTML = empleado[propiedad];
+                        tr.appendChild(td);
+                    }
+                }
+                let botonEditar = document.createElement("td");
+                    tr.appendChild(botonEditar);
 
-                    tablaDatos.appendChild(tr)
+                    botonEditar.innerHTML= 
 
-                }) //-------- "tbody" creado
+                  `<!-- Button to Open the Modal -->
+                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal" id="botonModificar">
+                    Open modal
+                  </button>
+                
+                  <!-- The Modal -->
+                  <div class="modal" id="myModal">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                      
+                        <!-- Modal Header -->
+                        <div class="modal-header">
+                          <h4 class="modal-title">Modal Heading</h4>
+                          <button type="button"  id ="cerrarModificar" class="close" data-dismiss="modal">&times;</button>
+                        </div>
+                        
+                        <!-- Modal body -->
+                        <div class="modal-body">
+                          Modal body..
+                        </div>
+                        
+                        <!-- Modal footer -->
+                        <div class="modal-footer">
+                          <button type="button" id="cerrarModificar1" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </div>`
+                    shadowRoot.querySelector("#botonModificar").addEventListener("click",function (){tr.this.verModal()})
+                    shadowRoot.querySelector("#cerrarModificar").addEventListener("click",()=>this.cerrarModal())
+                    shadowRoot.querySelector("#cerrarModificar1").addEventListener("click",()=>this.cerrarModal())
+
+                    
 
             })
 
-        //MODAL Botón Añadir
-       // let btnAdd = shadowRoot.querySelector("#btnAdd")
-       //btnAdd.addEventListener("click", ()=> this.formularioAñadir)
-       //btnAdd.addEventListener("click", () => alert("AÑADIR"))
-
+        })
     }
-     verModal(){
+    verModal(){
         console.log("verModal");
         this.shadowRoot.querySelector("#myModal").style.display = 'block'
     }
-    
-    formularioAñadir() {
-        console.log("HOLA, MODAL?")
-        let exampleModal = shadowRoot.querySelector("#exampleModal")
-        console.log(exampleModal)
-        //  exampleModal.toggle()
+
+    cerrarModal(){
+        console.log("verModal");
+        this.shadowRoot.querySelector("#myModal").style.display = 'none'
     }
+
+
 }
 
+window.customElements.define("wc-empleados", WCEmpleados);
+
+
+
+//Funcion que obtiene los datos
 function getDatos(url) {
     return new Promise(function (resolve, reject) {
         fetch(url)
@@ -175,4 +241,6 @@ function getDatos(url) {
 }
 
 
-window.customElements.define("wc-empleados", WCEmpleados);
+/*
+        
+    */
