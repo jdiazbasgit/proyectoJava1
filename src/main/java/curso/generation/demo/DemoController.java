bf0c5ab323f3807b259940bcb741d9bb40596e1d
Tu Nombre <you@example.com>
2021-03-08 13:21:43 +0100

    cambio a boot

---
+++ src/main/java/curso/generation/demo/DemoController.java
@@ -0,0 +1,41 @@
+package curso.generation.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import curso.generation.demo.entidades.Day;
+import curso.generation.demo.repositorios.DaysCRUDRepository;
+
+@Controller
+public class DemoController {
+	
+	
+	@Autowired
+	private DaysCRUDRepository repository;
+	
+	
+	@RequestMapping("/jornadas")
+	public String verDatos() {
+		
+		
+		Iterable<Day> jornadas=getRepository().getJornadasConUno(false);
+		for (Day day : jornadas) {
+			System.out.println(day.getDescripcion());
+		}
+		return "pepe";
+	}
+
+
+	public DaysCRUDRepository getRepository() {
+		return repository;
+	}
+
+
+	public void setRepository(DaysCRUDRepository repository) {
+		this.repository = repository;
+	}
+
+}
