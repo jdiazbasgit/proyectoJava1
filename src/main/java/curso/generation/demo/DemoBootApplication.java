7b4dd8489439f860605ebe14126ab43fcac790cd
Tu Nombre <you@example.com>
2021-03-09 22:46:37 +0100

    servlet

--- src/main/java/curso/generation/demo/DemoBootApplication.java
+++ src/main/java/curso/generation/demo/DemoBootApplication.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import curso.generation.demo.entidades.Day;
 import curso.generation.demo.repositorios.DaysCRUDRepository;
 
 @SpringBootApplication
-public class DemoBootApplication {
+public class DemoBootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoBootApplication.class, args);
