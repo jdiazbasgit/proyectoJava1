f0fde6f63c30d70d4cafb89cc82a15ab23bc5e8b
Tu Nombre <you@example.com>
2021-03-11 11:07:44 +0100

    subida error

--- src/main/java/curso/generation/demo/DemoController.java
+++ src/main/java/curso/generation/demo/DemoController.java
@@ -1,13 +1,7 @@
 package curso.generation.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import curso.generation.demo.entidades.Day;
-import curso.generation.demo.repositorios.DaysCRUDRepository;
 
 @Controller
 public class DemoController {
@@ -25,13 +19,6 @@ public class DemoController {
 	}
 	
 
-	public DaysCRUDRepository getRepository() {
-		return repository;
-	}
-
-
-	public void setRepository(DaysCRUDRepository repository) {
-		this.repository = repository;
-	}
+	
 
 }
