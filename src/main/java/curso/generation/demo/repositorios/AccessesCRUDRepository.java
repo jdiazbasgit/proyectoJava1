7b39280d01b43ccafa0216746d0dbf20de2708cb
Guiu <Guiu@PC-Guiu>
2021-03-11 11:04:07 +0100

    test

--- src/main/java/curso/generation/demo/repositorios/AccessesCRUDRepository.java
+++ src/main/java/curso/generation/demo/repositorios/AccessesCRUDRepository.java
@@ -7,6 +7,6 @@ import curso.generation.demo.entidades.Access;
 
 public interface AccessesCRUDRepository extends CrudRepository<Access, Integer> {
 
-		@Query(value ="from Access as a  where a.month=:mes and a.year=:anio")
+		@Query(value ="from Access as a where a.month=:mes and a.year=:anio")
 		public Iterable<Access> getAccessByAnioAndMes(int anio, int mes);
 }
