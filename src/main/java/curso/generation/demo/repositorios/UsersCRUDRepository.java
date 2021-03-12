c52e7d6c30c0badbf890a810a194e4cc01b7c047
Guiu <Guiu@PC-Guiu>
2021-03-12 11:01:51 +0100

    Override

--- src/main/java/curso/generation/demo/repositorios/UsersCRUDRepository.java
+++ src/main/java/curso/generation/demo/repositorios/UsersCRUDRepository.java
@@ -1,5 +1,7 @@
 package curso.generation.demo.repositorios;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
@@ -10,5 +12,14 @@ public interface UsersCRUDRepository extends CrudRepository<User, Integer> {
 	@Query("from User u where u.user=:usuario and u.password=:clave")
 	public User getUserByUsuarioAndClave(String usuario, String clave);
 
+	@Override
+	default Optional<User> findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	default Iterable<User> findAll() {
+		return null;
+	}
 }
