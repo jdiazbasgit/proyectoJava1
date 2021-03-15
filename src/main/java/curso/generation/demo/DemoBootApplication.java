a9bed1a1a203b9a6d1feee6c9c9a9233793f07c3
Tu Nombre <you@example.com>
2021-03-15 10:26:16 +0100

    subida

--- src/main/java/curso/generation/demo/DemoBootApplication.java
+++ src/main/java/curso/generation/demo/DemoBootApplication.java
@@ -1,14 +1,8 @@
 package curso.generation.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import curso.generation.demo.entidades.Day;
-import curso.generation.demo.repositorios.DaysCRUDRepository;
 
 @SpringBootApplication
 //@EnableWebMvc 
