aa4d22308bed73613d974242a60834fab4b9d4ff
Jizga <jizga93@gmail.com>
2021-03-15 19:40:05 +0100

    cosas innecesarias borradas

--- src/main/java/curso/generation/demo/repositorios/EmployeeCRUDRepository.java
+++ src/main/java/curso/generation/demo/repositorios/EmployeeCRUDRepository.java
@@ -11,9 +11,5 @@ import curso.generation.demo.entidades.Employee;
 @Repository
 public interface EmployeeCRUDRepository extends CrudRepository<Employee, Integer> {
 
-	@Query("from Employee as E where E.nombre=:nombre and E.apellidos=:apellidos and E.dni=:dni and E.identificador=:identificador and E.fecha_alta=:fecha_alta")
-	public Employee getEmployeeAlta(String nombre, String apellidos, String dni, String identificador, String fecha_alta);
-
-	@Query("from Employee as E where E.nombre=:nombre and E.apellidos=:apellidos and E.dni=:dni and E.identificador=:identificador and E.fecha_alta=:fecha_alta and E.fecha_baja=:fecha_baja")
-	public Employee getEmployeeModificar(String nombre, String apellido, String dni, String identificador, String fecha_alta, String fecha_baja);
+	
 }
