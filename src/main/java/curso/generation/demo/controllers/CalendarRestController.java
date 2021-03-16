bf6277d2678082198fb3c9fb06f891f93e802fb3
Tu Nombre <you@example.com>
2021-03-16 11:42:22 +0100

    a

--- src/main/java/curso/generation/demo/controllers/CalendarRestController.java
+++ src/main/java/curso/generation/demo/controllers/CalendarRestController.java
@@ -5,6 +5,7 @@ import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,7 +45,7 @@ public class CalendarRestController {
 
 	}
 	
-	@PostMapping("api/users")
+	@GetMapping("api/users")
 	public Iterable<User> getCalendarRest1() {
 		
 		return getRepositoryUsers().findAll();
