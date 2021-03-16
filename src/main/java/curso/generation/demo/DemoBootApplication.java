8e5e2dc173490066532858f8221f455f73da869c
Tu Nombre <you@example.com>
2021-03-16 10:55:24 +0100

    cors

--- src/main/java/curso/generation/demo/DemoBootApplication.java
+++ src/main/java/curso/generation/demo/DemoBootApplication.java
@@ -1,16 +1,15 @@
 package curso.generation.demo;
 
-import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import curso.generation.demo.filters.JWTAuthorizationFilter;
 
@@ -26,17 +25,15 @@ public class DemoBootApplication extends SpringBootServletInitializer {
 	@Configuration
 	@EnableWebSecurity
 	@EnableAspectJAutoProxy
+	@CrossOrigin(origins = "*")
 	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			http.csrf().disable()
 					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-					.authorizeRequests()
-					.antMatchers("/user").permitAll()
-					.antMatchers("/login").permitAll()
-					.antMatchers("/").permitAll()
-					.anyRequest().authenticated();
+					.authorizeRequests().antMatchers("/user").permitAll().antMatchers("/login").permitAll()
+					.antMatchers("/").permitAll().anyRequest().authenticated();
 		}
 
 	}
