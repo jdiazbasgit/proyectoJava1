5b00bc58046b08c02842c7bf16ccc4f9bbdf8c08
Tu Nombre <you@example.com>
2021-03-16 13:02:38 +0100

    a

--- src/main/java/curso/generation/demo/DemoBootApplication.java
+++ src/main/java/curso/generation/demo/DemoBootApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
